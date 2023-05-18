d88ae8ed9784800c40891a993490df93191585ae
roomhackerfrom100 <megamen932@gmai.com>
2023-05-18 20:01:06 +0300

    requirements update

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.redis import RedisStorage, RedisStorage2
 from decouple import config
 import openai
 TELEGRAM_BOT_TOKEN = config('TELEGRAM_BOT_TOKEN')
 CHATGPT_API_KEY = config('CHATGPT_API_KEY')
 openai.api_key = CHATGPT_API_KEY
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot)
+storage=RedisStorage2(bot=bot)
+dp = Dispatcher(bot,storage=storage)
